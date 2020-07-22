dba9b0758c76336ebff96e2101f438190235472b
Till Brede <tb@symcon.de>
2020-07-22 14:40:13 +0200

    fixed style

--- Treppenhauslichtsteuerung/module.php
+++ Treppenhauslichtsteuerung/module.php
@@ -55,7 +55,7 @@ class Treppenhauslichtsteuerung extends IPSModule
                 return false;
             }
         };
-        
+
         $legacyUpdateInput = $transferProperty('InputTriggerID', 'InputTriggers');
         $legacyUpdateOutput = $transferProperty('OutputID', 'OutputVariables');
 
